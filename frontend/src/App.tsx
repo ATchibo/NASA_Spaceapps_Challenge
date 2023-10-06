dd23eb9ccae2c1248e2bd2dd58ed72b2582317d7
Andrei Cibotariu <andreicibotariu@outlook.com>
2023-10-06 12:46:06 +0300

    installed flowbyte

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,29 @@
+import {Card, Datepicker, TextInput} from 'flowbite-react';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="h-screen flex justify-center items-center flex-col">
+        <h1 className="text-2xl font-bold text-blue-600 mb-8 border-b-4 border-purple-600">
+          Install Vite + React + Flowbite
+        </h1>
+        <div className="max-w-sm">
+          <Card imgSrc="https://flowbite.com/docs/images/blog/image-1.jpg">
+            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              Install & setup flowbite in react js
+            </h5>
+            <p className="font-normal text-gray-700 dark:text-gray-400">
+              Here are the biggest enterprise technology acquisitions of 2021 so
+              far, in reverse chronological order.
+            </p>
+              <TextInput>
+
+              </TextInput>
+
+              <Datepicker />
+          </Card>
+        </div>
+      </div>
   );
 }
 
