a7312c6f64a792bd74ca7be7d24070f860131c35
Andrei Cibotariu <andreicibotariu@outlook.com>
2023-10-07 16:26:05 +0300

    new websock implementation

--- frontend/src/components/MainChat.tsx
+++ frontend/src/components/MainChat.tsx
@@ -23,32 +23,7 @@ const MainChat = () => {
     }]);
 
     const [currentMessage, setCurrentMessage] = useState("");
-
-    /**
-     * Websocket Connection
-     * */
-
-    const [socket, setSocket] = useState<WebSocket>();
-    const [receivedMsg, setReceivedMsg] = useState("");
-    const [receivingMsg, setReceivingMsg] = useState(false);
-
-    useEffect(() => {
-        // connect to WebSocket server
-
-        // const socket = new WebSocket('ws://localhost:5000/echo');
-        const socket = new WebSocket('wss://316c-35-203-157-221.ngrok-free.app/queue/join');
-        socket.addEventListener('message', ev => {
-            if (ev.data === "$$$") {
-                // end of message
-                setReceivedMsg("");
-                setReceivingMsg(false);
-            } else {
-                console.log(ev.data);
-                setReceivedMsg(receivedMsg + ev.data);
-            }
-        });
-        setSocket(socket);
-    }, []);
+    const [msgToSend, setMsgToSend] = useState("");
 
     const sendMessage = (event: any) => {
         event.preventDefault();
@@ -56,14 +31,347 @@ const MainChat = () => {
         if (currentMessage.length === 0)
             return;
 
-        if (socket)
-            socket.send(currentMessage);
         setMessageQueue([...messageQueue, {sender: SENDER_TYPE_USER, content: currentMessage}, {sender: SENDER_TYPE_BOT, content: ""}]);
         setReceivingMsg(true);
+        setMsgToSend(currentMessage);
         setCurrentMessage("");
+
+        startSendingMessage();
     }
 
+    /**
+     * Websocket Connection
+     * */
+
+    const [socket, setSocket] = useState<WebSocket>();
+    const [fnIndex, setFnIndex] = useState(36);
+    const [receivedMsg, setReceivedMsg] = useState("");
+    const [receivingMsg, setReceivingMsg] = useState(false);
+
+    const connectToWebsocket = () => {
+        const socket = new WebSocket('wss://316c-35-203-157-221.ngrok-free.app/queue/join');
+        setSocket(socket);
+    }
+
+    const sessionHash = "ifonas1sp2";
+
+    const step1 = () => {
+        var currentFnIndex = fnIndex;
+        setFnIndex(currentFnIndex + 1);
+
+        // step 1/7
+        connectToWebsocket();
+        socket?.addEventListener('message', ev => {
+            const dataString = JSON.stringify(ev.data);
+
+            if (dataString === "{\"msg\":\"send_hash\"}\n") {
+                socket?.send(JSON.stringify({
+                    fn_index: currentFnIndex,
+                    session_hash: sessionHash
+                }));
+            } else if (dataString === "{\"msg\":\"send_data\"}\n") {
+                socket?.send(JSON.stringify({
+                    data: [msgToSend],
+                    event_data: null,
+                    fn_index: currentFnIndex,
+                    session_hash: sessionHash
+                }));
+            }
+        });
+        socket?.addEventListener('disconnect', ev => {
+            step2();
+        })
+    }
+
+    const step2 = () => {
+        var currentFnIndex = fnIndex;
+        setFnIndex(currentFnIndex + 1);
+
+        // step 2/7
+        connectToWebsocket();
+        socket?.send(JSON.stringify({
+            fn_index: currentFnIndex,
+            session_hash: sessionHash
+        }));
+
+        socket?.addEventListener('message', ev => {
+            const dataString = JSON.stringify(ev.data);
+
+            if (dataString === "{\"msg\":\"send_data\"}\n") {
+                socket?.send(JSON.stringify({
+                    data: [
+                        msgToSend,
+                        "",
+                        "",
+                        true,
+                        "human_bot",
+                        "{   'PreInput': None,\n    'PreInstruct': '<human>: ',\n    'PreResponse': '<bot>:',\n    'botstr': '<bot>:',\n    'chat_sep': '\\n',\n    'chat_turn_sep': '\\n',\n    'generates_leading_space': True,\n    'humanstr': '<human>:',\n    'promptA': '',\n    'promptB': '',\n    'terminate_response': [   '\\n<human>:',\n                              '\\n<bot>:',\n                              '<human>:',\n                              '<bot>:',\n                              '<bot>:']}",
+                        0.1,
+                        0.75,
+                        40,
+                        1,
+                        256,
+                        0,
+                        false,
+                        180,
+                        1.07,
+                        1,
+                        false,
+                        true,
+                        "",
+                        "",
+                        "ChatLLM",
+                        "Query",
+                        3,
+                        true,
+                        512,
+                        [
+                            "All_Relevant"
+                        ],
+                        []
+                    ],
+                    event_data: null,
+                    fn_index: currentFnIndex,
+                    session_hash: sessionHash
+                }));
+            }
+        });
+        socket?.addEventListener('disconnect', ev => {
+            step3();
+        })
+    }
+
+    const step3 = () => {
+        var currentFnIndex = fnIndex;
+        setFnIndex(currentFnIndex + 1);
+
+        // step 3/7
+        connectToWebsocket();
+        socket?.addEventListener('message', ev => {
+            const dataString = JSON.stringify(ev.data);
+
+            if (dataString === "{\"msg\":\"send_hash\"}\n") {
+                socket?.send(JSON.stringify({
+                    fn_index: currentFnIndex,
+                    session_hash: sessionHash
+                }));
+            } else if (dataString === "{\"msg\":\"send_data\"}\n") {
+                socket?.send(JSON.stringify({
+                    data: [],
+                    event_data: null,
+                    fn_index: currentFnIndex,
+                    session_hash: sessionHash
+                }));
+            }
+        });
+        socket?.addEventListener('disconnect', ev => {
+            step4();
+        })
+    }
+
+    const step4 = () => {
+        var currentFnIndex = fnIndex;
+        setFnIndex(currentFnIndex + 1);
+
+        // step 3/7
+        connectToWebsocket();
+        socket?.send(JSON.stringify({
+            fn_index: currentFnIndex,
+            session_hash: sessionHash
+        }));
+
+        socket?.addEventListener('message', ev => {
+            const dataString = JSON.stringify(ev.data);
+
+            if (dataString === "{\"msg\":\"send_data\"}\n") {
+                socket?.send(JSON.stringify({
+                    data: [
+                        msgToSend,
+                        "",
+                        "",
+                        true,
+                        "human_bot",
+                        "{   'PreInput': None,\n    'PreInstruct': '<human>: ',\n    'PreResponse': '<bot>:',\n    'botstr': '<bot>:',\n    'chat_sep': '\\n',\n    'chat_turn_sep': '\\n',\n    'generates_leading_space': True,\n    'humanstr': '<human>:',\n    'promptA': '',\n    'promptB': '',\n    'terminate_response': [   '\\n<human>:',\n                              '\\n<bot>:',\n                              '<human>:',\n                              '<bot>:',\n                              '<bot>:']}",
+                        0.1,
+                        0.75,
+                        40,
+                        1,
+                        256,
+                        0,
+                        false,
+                        180,
+                        1.07,
+                        1,
+                        false,
+                        true,
+                        "",
+                        "",
+                        "ChatLLM",
+                        "Query",
+                        3,
+                        true,
+                        512,
+                        [
+                            "All_Relevant"
+                        ],
+                        []
+                    ],
+                    event_data: null,
+                    fn_index: currentFnIndex,
+                    session_hash: sessionHash
+                }));
+            }
+        });
+        socket?.addEventListener('disconnect', ev => {
+            step5();
+        })
+    }
+
+    const step5 = () => {
+        var currentFnIndex = fnIndex;
+        setFnIndex(currentFnIndex + 1);
+
+        // step 3/7
+        connectToWebsocket();
+        socket?.send(JSON.stringify({
+            fn_index: currentFnIndex,
+            session_hash: sessionHash
+        }));
+
+        socket?.addEventListener('message', ev => {
+            const dataString = JSON.stringify(ev.data);
+
+            if (dataString === "{\"msg\":\"send_data\"}\n") {
+                socket?.send(JSON.stringify({
+                    data: [],
+                    event_data: null,
+                    fn_index: currentFnIndex,
+                    session_hash: sessionHash
+                }));
+            }
+        });
+        socket?.addEventListener('disconnect', ev => {
+            step6();
+        })
+    }
+
+    const step6 = () => {
+        var currentFnIndex = fnIndex;
+        setFnIndex(currentFnIndex + 1);
+
+        // step 3/7
+        connectToWebsocket();
+        socket?.send(JSON.stringify({
+            fn_index: currentFnIndex,
+            session_hash: sessionHash
+        }));
+
+        socket?.addEventListener('message', ev => {
+            const dataString = JSON.stringify(ev.data);
+
+            if (dataString === "{\"msg\":\"send_data\"}\n") {
+                socket?.send(JSON.stringify({
+                    data: [],
+                    event_data: null,
+                    fn_index: currentFnIndex,
+                    session_hash: sessionHash
+                }));
+            }
+        });
+        socket?.addEventListener('disconnect', ev => {
+            step7();
+        })
+    }
+
+    const step7 = () => {
+        var currentFnIndex = fnIndex;
+        setFnIndex(currentFnIndex + 1);
+
+        // step 3/7
+        connectToWebsocket();
+        socket?.send(JSON.stringify({
+            fn_index: currentFnIndex,
+            session_hash: sessionHash
+        }));
+
+        socket?.addEventListener('message', ev => {
+            const dataString = JSON.stringify(ev.data);
+
+            if (dataString === "{\"msg\":\"send_data\"}\n") {
+                socket?.send(JSON.stringify({
+                    data: [
+                        msgToSend,
+                        "",
+                        "",
+                        true,
+                        "human_bot",
+                        "{   'PreInput': None,\n    'PreInstruct': '<human>: ',\n    'PreResponse': '<bot>:',\n    'botstr': '<bot>:',\n    'chat_sep': '\\n',\n    'chat_turn_sep': '\\n',\n    'generates_leading_space': True,\n    'humanstr': '<human>:',\n    'promptA': '',\n    'promptB': '',\n    'terminate_response': [   '\\n<human>:',\n                              '\\n<bot>:',\n                              '<human>:',\n                              '<bot>:',\n                              '<bot>:']}",
+                        0.1,
+                        0.75,
+                        40,
+                        1,
+                        256,
+                        0,
+                        false,
+                        180,
+                        1.07,
+                        1,
+                        false,
+                        true,
+                        "",
+                        "",
+                        "ChatLLM",
+                        "Query",
+                        3,
+                        true,
+                        512,
+                        [
+                            "All_Relevant"
+                        ],
+                        []
+                    ],
+                    event_data: null,
+                    fn_index: currentFnIndex,
+                    session_hash: sessionHash
+                }));
+            } else if (dataString === "{\"msg\":\"process_starts\"}") {
+                setReceivedMsg("");
+                setReceivingMsg(false);
+            } else if (dataString.includes("{\"msg\":\"process_generating\"")) {
+                setReceivedMsg(receivedMsg + ev.data.output.data[0][0][1]);
+            }
+        });
+        socket?.addEventListener('disconnect', ev => {
+            console.log("gata bos")
+        })
+    }
+
+    const startSendingMessage = () => {
+        step1();
+    }
+
+    // useEffect(() => {
+    //     // connect to WebSocket server
+    //
+    //     // const socket = new WebSocket('ws://localhost:5000/echo');
+    //     const socket = new WebSocket('wss://316c-35-203-157-221.ngrok-free.app/queue/join');
+    //     socket.addEventListener('message', ev => {
+    //         if (ev.data === "$$$") {
+    //             // end of message
+    //             setReceivedMsg("");
+    //             setReceivingMsg(false);
+    //         } else {
+    //             console.log(ev.data);
+    //             setReceivedMsg(receivedMsg + ev.data);
+    //         }
+    //     });
+    //     setSocket(socket);
+    // }, []);
+
     useEffect(() => {
+
+        console.log("Received msg: ", receivedMsg);
+
         if (receivedMsg.length > 0) {
             setMessageQueue([...messageQueue.slice(0, messageQueue.length-1), {sender: SENDER_TYPE_BOT, content: receivedMsg}]);
         }
